27c96c19f14e63434cfd5c5e9ce0275e39046f42
bvvivek6 <vivekbv242813@gmail.com>
2025-06-22 11:19:21 +0530

    Refactor JumpToSection component to enhance animation effects and improve styling

--- src/components/JumpToSection.jsx
+++ src/components/JumpToSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { HiOutlineMenu } from "react-icons/hi";
 import { HiOutlineX } from "react-icons/hi";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const sectionList = [
   { key: "about", label: "Overview" },
@@ -14,40 +14,60 @@ const JumpToSection = ({ onJump }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div
-      className={`fixed top-6 left-6 z-50 flex items-center h-10 ${
-        open ? "bg-[#4b4b4b] text-white" : ""
-      } shadow-inner backdrop-blur-lg border text-black border-gray-200 rounded-full overflow-visible cursor-pointer `}
-    >
+    <div className="fixed top-6 left-6 z-50 flex items-center h-10  shadow-inner backdrop-blur-lg border text-black border-gray-200 rounded-full overflow-visible cursor-pointer ">
       <button
         className="w-10 h-10 flex  items-center justify-center text-xs font-light "
         onClick={() => setOpen((v) => !v)}
       >
         {open ? <HiOutlineX size={20} /> : <HiOutlineMenu size={20} />}
       </button>
-      {open && (
-        <motion.div
-          className="absolute top-12 left-0 grid grid-cols-2 min-w-[220px] gap-1
-          flex-col"
-          initial={{ opacity: 0, y: -10, filter: "blur(10px)" }}
-          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-          transition={{ duration: 0.4, ease: "easeInOut" }}
-        >
-          {sectionList.map((section) => (
-            <button
-              key={section.key}
-              className="text-center px-4 py-3 font-sans shadow-neutral-100 shadow-lg
-               tracking-tight bg-[#4b4b4b] border border-gray-200 text-sm rounded-2xl hover:bg-gray-100 w-full text-white"
-              onClick={() => {
-                setOpen(false);
-                if (onJump) onJump(section.key);
-              }}
-            >
-              {section.label}
-            </button>
-          ))}
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            className="absolute top-8 left-6 grid grid-cols-2 min-w-[220px] gap-1"
+            initial={{
+              opacity: 0.3,
+              x: -120,
+              y: -50,
+              scale: 0.1,
+              filter: "blur(10px)",
+            }}
+            animate={{
+              opacity: 1,
+              x: -20,
+              y: 20,
+              scale: 1,
+              filter: "blur(0px)",
+            }}
+            exit={{
+              opacity: 0,
+              x: -120,
+              y: -50,
+              scale: 0.1,
+              filter: "blur(10px)",
+            }}
+            transition={{
+              type: "spring",
+              stiffness: 120,
+              damping: 18,
+            }}
+          >
+            {sectionList.map((section) => (
+              <button
+                key={section.key}
+                className="text-center px-4 py-3 font-sans shadow-neutral-100 shadow-inner
+           tracking-tight bg-[#ffffff] border border-gray-200 text-sm rounded-2xl hover:bg-gray-200 w-full text-gray-900"
+                onClick={() => {
+                  setOpen(false);
+                  if (onJump) onJump(section.key);
+                }}
+              >
+                {section.label}
+              </button>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
