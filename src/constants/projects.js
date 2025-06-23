136ada146bb6b095cb4f8efe9481ee92b15c87f5
bvvivek6 <vivekbv242813@gmail.com>
2025-06-23 13:29:02 +0530

    Add Projects component and project constants with details

---
+++ src/constants/projects.js
@@ -0,0 +1,47 @@
+const projects = [
+  {
+    title: "Time Capsule",
+    description:
+      "A full-stack digital vault enabling users to securely store and schedule the delivery of text, images, and videos. Features user authentication, media uploads, timed content access, and email reminders for engagement.",
+    techStack: [
+      "React",
+      "Tailwind CSS",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "cron jobs",
+      "JWT",
+      "Nodemailer",
+      "Cloudinary",
+    ],
+    github: "",
+    demo: "",
+    image: "timecapsule.png",
+  },
+  {
+    title: "AlgoFlow",
+    description:
+      "An interactive web app to visualize and learn sorting algorithms with step-by-step animations and pseudocode. Integrates dynamic resource suggestions using YouTube Data V3 API and features a modern, responsive UI.",
+    techStack: ["React.js", "Tailwind CSS"],
+    github: "",
+    demo: "",
+    image: "algoflow.png",
+  },
+  {
+    title: "Snapster",
+    description:
+      "A fun and interactive photo booth app with real-time webcam capture, creative filters, countdown timer, collage generation, and download functionality. Features sleek animations using framer motion.",
+    techStack: [
+      "React",
+      "Tailwind CSS",
+      "HTML5 Canvas",
+      "WebRTC",
+      "framer-motion",
+    ],
+    github: "",
+    demo: "",
+    image: "snapster.png",
+  },
+];
+
+export default projects;
